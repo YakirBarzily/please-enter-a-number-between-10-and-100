2fca015493293bdc6c5604793454f5283f6d7a75
User <User@COMP-6>
2022-02-10 16:18:41 +0200

    hi

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number between 10 and 100");
+            int x;
+            x = int.Parse(Console.ReadLine());
+            int y;
+            y = (x / 10);
+            int q;
+            q = (x % 10);
+            if (y == q)
+                Console.WriteLine("ok");
+            else
+                Console.WriteLine("error");
+        }
+    }
+}
